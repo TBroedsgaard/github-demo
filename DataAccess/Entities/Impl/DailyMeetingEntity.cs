4d78c787cbc6c8171e4ce85c4543ef5fe450f250
Klockz <lasse.bredgaard@gmail.com>
2014-01-13 12:20:46 +0100

    DailyMeetingEntity
    impl meeting entity

---
+++ DataAccess/Entities/Impl/DailyMeetingEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities.Impl
+{
+    public class DailyMeetingEntity : IDailyMeetingEntity
+    {
+
+        public DateTime date {get; set;}
+        public string todoYesterday {get; set;}
+        public string todoToday {get; set;}
+        public string roadblocks {get; set;}
+        public IUserEntity user {get; set;}
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,9 @@ github-demo
 ===========
 
 Hands on demo
+
+<html>
+<body>
+<header> LULZ </header>
+</body>
+</html>
